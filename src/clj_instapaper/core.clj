594b0edd3802db6e6523f17a8589195f6e461344
Dennis Laumen <dennis@lau.mn>
2012-09-01 15:14:42 +0200

    Clarified use of authenticate function in documentation.

--- src/clj_instapaper/core.clj
+++ src/clj_instapaper/core.clj
@@ -2,7 +2,14 @@
   (:require [clj-http.client :as c]))
 
 (defn authenticate
-  "This method validates an Instapaper username and password. Calling this before adding pages is not necessary. Use this if you want to only check credentials without adding a URL to Instapaper, such as when you first prompt the user for Instapaper credentials in a settings screen or on the first Instapaper request."
+  "Returns true if the username and password are correct and false if they're 
+  not. Exceptions with HTTP response details are thrown all other cases.
+  
+  This method validates an Instapaper username and password. Calling this
+  before adding pages is not necessary. Use this if you want to only check
+  credentials without adding a URL to Instapaper, such as when you first prompt
+  the user for Instapaper credentials in a settings screen or on the first
+  Instapaper request."
   [username password]
   (let [http-response (c/post "https://www.instapaper.com/api/authenticate" {:basic-auth [username password] :throw-exceptions false})]
     (case (http-response :status)

82630fc2c40dd9ea8cbad9ad4d3c1c4f78a2f020
Dennis Laumen <dennis@lau.mn>
2012-09-01 12:06:51 +0200

    Updated the authenticate method to support Instapaper's the documented HTTP codes.

--- src/clj_instapaper/core.clj
+++ src/clj_instapaper/core.clj
@@ -4,6 +4,10 @@
 (defn authenticate
   "This method validates an Instapaper username and password. Calling this before adding pages is not necessary. Use this if you want to only check credentials without adding a URL to Instapaper, such as when you first prompt the user for Instapaper credentials in a settings screen or on the first Instapaper request."
   [username password]
-  (c/post "https://www.instapaper.com/api/authenticate"
-    {:basic-auth [username password]}))
+  (let [http-response (c/post "https://www.instapaper.com/api/authenticate" {:basic-auth [username password] :throw-exceptions false})]
+    (case (http-response :status)
+      200 true
+      403 (throw (new Exception "Invalid username or password."))
+      500 (throw (new Exception "The service encountered an error. Please try again later."))
+      throw (new Exception (str "Unexpected HTTP response: " http-response)))))
 
--- test/clj_instapaper/test/core.clj
+++ test/clj_instapaper/test/core.clj
@@ -1,13 +1,26 @@
 (ns clj-instapaper.test.core
-  (:use [clj-instapaper.core])
-  (:use [midje.sweet])
-  (:require [clj-http.client])
-  (:use clj-http.fake))
+  (:use clj-http.fake)
+  (:use clj-instapaper.core)
+  (:use midje.sweet))
 
 (with-fake-routes {
-    "https://www.instapaper.com/api/authenticate" (fn [request] {:status 200 :headers {} :body "Welcome!"})
+  "https://www.instapaper.com/api/authenticate"
+  (fn [request]
+    (let [auth-header ((request :headers) "Authorization") marco-arment "Basic TWFyY286QXJtZW50" nate-weiner "Basic TmF0ZTpXZWluZXI=" rich-ziade "Basic UmljaDpaaWFkZQ=="]
+      (cond
+        (= auth-header marco-arment)
+          {:status 200 :headers {} :body "OK"}
+        (= auth-header nate-weiner)
+          {:status 403 :headers {} :body "Invalid username or password."}
+        (= auth-header rich-ziade)
+          {:status 500 :headers {} :body "The service encountered an error. Please try again later."}
+        :else
+          {:status 503 :headers {} :body "Service Unavailable"})))
   }
-                  
-  (fact 
-    (authenticate "marco" "arment") => (contains {:status 200})))
+
+  (fact "Authentication"
+    (authenticate "Marco" "Arment") => true
+    (authenticate "Nate" "Weiner") => (throws "Invalid username or password.")
+    (authenticate "Rich" "Ziade") => (throws "The service encountered an error. Please try again later.")
+    (authenticate "Tim" "Cook") => throws))
 

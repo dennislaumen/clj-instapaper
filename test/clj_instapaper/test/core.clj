ec0e4acbb1b2ed7628070e069ae91fe183c18cc0
Dennis Laumen <dennis@lau.mn>
2012-09-01 15:38:12 +0200

    Simplified tests by making HTTP 200 the default response for authentication.

--- test/clj_instapaper/test/core.clj
+++ test/clj_instapaper/test/core.clj
@@ -6,14 +6,14 @@
 (with-fake-routes {
   "https://www.instapaper.com/api/authenticate"
   (fn [request]
-    (let [auth-header ((request :headers) "Authorization") marco-arment "Basic TWFyY286QXJtZW50" nate-weiner "Basic TmF0ZTpXZWluZXI=" rich-ziade "Basic UmljaDpaaWFkZQ=="]
+    (let [auth-header ((request :headers) "Authorization") nate-weiner "Basic TmF0ZTpXZWluZXI=" rich-ziade "Basic UmljaDpaaWFkZQ=="]
       (cond
-        (= auth-header marco-arment)
-          {:status 200 :headers {} :body "OK"}
         (= auth-header nate-weiner)
           {:status 403 :headers {} :body "Invalid username or password."}
         (= auth-header rich-ziade)
-          {:status 500 :headers {} :body "The service encountered an error. Please try again later."})))
+          {:status 500 :headers {} :body "The service encountered an error. Please try again later."}
+        :else
+          {:status 200 :headers {} :body "OK"})))
   }
 
   (fact "Authentication"

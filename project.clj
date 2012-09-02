9e62bab82c72138da47469fe1a50d8790ce6195e
Dennis Laumen <dennis@lau.mn>
2012-09-02 09:16:26 +0200

    Updated clj-http dependency to 0.5.3.
     * Changed handling of authentication headers after change in clj-http.
     * Updated tests with an extra fact to get clearer feedback if similar
       things change in the future.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Clojure implementation of the Instapaper Simple Developer API."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [midje "1.4.0"]
-                 [clj-http "0.4.3"]
+                 [clj-http "0.5.3"]
                  [clj-http-fake "0.4.1"]]
   :dev-dependencies [[lein-midje "1.0.10"]])
--- test/clj_instapaper/test/core.clj
+++ test/clj_instapaper/test/core.clj
@@ -6,11 +6,12 @@
 (with-fake-routes {
   "https://www.instapaper.com/api/authenticate"
   (fn [request]
-    (let [auth-header ((request :headers) "Authorization") 
+    (let [auth-header ((request :headers) "authorization") 
           nate-weiner "Basic TmF0ZTpXZWluZXI=" 
-          rich-ziade "Basic UmljaDpaaWFkZQ=="]
+          rich-ziade "Basic UmljaDpaaWFkZQ=="
+          nate "Basic TmF0ZTo="]
       (cond
-        (= auth-header nate-weiner)
+        (or (= auth-header nate-weiner) (= auth-header nate))
           {:status 403 :headers {} :body "Invalid username or password."}
         (= auth-header rich-ziade)
           {:status 500 :headers {} :body "The service encountered an error. Please try again later."}
@@ -21,5 +22,6 @@
     (authenticate "Marco" "Arment") => true
     (authenticate "Nate" "Weiner") => false 
     (authenticate "Rich" "Ziade") => throws
-    (authenticate "Marco") => true))
+    (authenticate "Marco") => true)
+    (authenticate "Nate") => false)
 
